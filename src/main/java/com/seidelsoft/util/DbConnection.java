eac4f8e7ea11efa75ed5c64077c4b0c0f9a4deaa
Luis Seidel <luis.seidel@celk.net>
2023-04-21 18:18:21 -0300

    feat: config properties file

--- src/main/java/com/seidelsoft/util/DbConnection.java
+++ src/main/java/com/seidelsoft/util/DbConnection.java
@@ -6,42 +6,10 @@ import java.sql.SQLException;
 
 public class DbConnection {
 
-    private static final String driver = "jdbc:postgresql";
-    private static final String host = "localhost";
-    private static final String port = "5432";
-    private static final String database = "testeServlet";
-    private static final String user = "celk";
-    private static final String password = "celk";
+    private static DbConfig configs;
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(getURL(), getUser(), getPassword());
-    }
-
-    private static String getURL() {
-        return getDriver() + "://" + getHost() + ":" + getPort() + "/" + getDatabase();
-    }
-
-    public static String getDriver() {
-        return driver;
-    }
-
-    public static String getHost() {
-        return host;
-    }
-
-    public static String getPort() {
-        return port;
-    }
-
-    public static String getDatabase() {
-        return database;
-    }
-
-    public static String getUser() {
-        return user;
-    }
-
-    public static String getPassword() {
-        return password;
+        configs = new DbConfig();
+        return DriverManager.getConnection(configs.getURL(), configs.getUser(), configs.getPassword());
     }
 }
